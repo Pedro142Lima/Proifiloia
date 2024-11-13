a5c54731a907a076711d1e1f4aca966dcac1544e
Pedro <pedroeinviller142gmail.com>
2024-11-13 18:16:51 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Land from './pages/land-page';
+
+function App() {
+  return (
+    <div>
+      <Land/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/land-page/index.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './index.scss'
+export default function Land () {
+    return (
+        <div className="main">
+            <header>
+                <a href="">Sobre</a>
+
+                <a href="">Portifólio</a>
+            </header>
+            <div className="section">
+                
+                <div className="box">
+                    <h2>Como o limão vira limonada, nós transformamos sua ideia, em um site de sucesso.</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
